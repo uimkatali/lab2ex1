d496056a1a62b384eb5e3f3011b1d5972a3428e9
uimkatali <b8bf6b5@gmail.com>
2023-09-25 18:25:23 +0300

    Add project files.

---
+++ lab2ex1/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+  Scrieti o functie care citeşte de la tastatură un şir de n numere naturale şi
+    determină media aritmetică a celor pare, n fiind citit de la tastatra
+ */
+
+
+namespace lab2ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = Convert.ToInt32(Console.In.ReadLine());
+            int[] array = new int[size];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt32(Console.In.ReadLine());
+
+            }
+            int sum = 0; 
+            for (int i = 0;i < array.Length; i++)
+            {
+                if (array[i] %2 == 0)
+                {
+                    Console.WriteLine(array[i]);
+                    sum += array[i];
+                }
+            }
+            Console.WriteLine(sum/size);
+        }
+    }
+}
